a8497ce4ae37bab86ae0d7a0f3f5b13c50b7c2c0
TavoDGant <61161002+TavoDGant@users.noreply.github.com>
2021-03-24 16:17:29 -0600

    Update Get

--- js/requests.js
+++ js/requests.js
@@ -1,14 +1,25 @@
+const tabla = document.querySelector("#list-products tbody");
+
 function traerDatos(){
+    cleanBody();
     fetch('https://spring-boot-mongo-db.herokuapp.com/allProducts')
     .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        recorrer(data);
+    .then(products => {
+        products.forEach(products => {
+            const row = document.createElement('tr');
+            var prodName = products.name;
+            var prodDesc = products.description;
+            row.innerHTML += `
+                <td>${prodName.replace(/\b\w/g, l => l.toUpperCase())}</td>
+                <td>${prodDesc.replace(/\b\w/g, l => l.toUpperCase())}</td>
+                <td>$ ${products.price}</td>
+            `;
+            tabla.appendChild(row);
+        });
     })
 }
 
-function recorrer(dat){
-    for (let i = 0; i < dat.length; i++) {
-        console.log(dat[i]);
-    }
+function cleanBody(){
+   var tabClear = document.querySelector("tbody");
+   tabClear.innerHTML = "";
 }
