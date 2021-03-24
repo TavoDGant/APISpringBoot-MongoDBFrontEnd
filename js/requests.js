43b7512a2ebb99e70a32225f12ae1ea6fd5d7b61
TavoDGant <61161002+TavoDGant@users.noreply.github.com>
2021-03-24 14:58:01 -0600

    Update Bootstrap

--- js/requests.js
+++ js/requests.js
@@ -1,6 +1,14 @@
 function traerDatos(){
     fetch('https://spring-boot-mongo-db.herokuapp.com/allProducts')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(data => {
+        console.log(data);
+        recorrer(data);
+    })
+}
 
+function recorrer(dat){
+    for (let i = 0; i < dat.length; i++) {
+        console.log(dat[i]);
+    }
 }
