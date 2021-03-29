a9c3a5e79e06529e622f343d08f3d7b1095e0b17
TavoDGant <61161002+TavoDGant@users.noreply.github.com>
2021-03-29 13:09:37 -0600

    Update popUp Modelo

--- js/requests.js
+++ js/requests.js
@@ -1,6 +1,17 @@
+/*-------------MODALS*---------------------*/
+var modalUpdate = document.getElementById("myModalUpdate");
+var modalAdd = document.getElementById("myModalAdd");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 const tabla = document.querySelector("#list-products tbody");
 const allTable = document.getElementById("list-products");
 const notFound = document.getElementById("message");
+
 
 document.getElementById("search-products").onkeypress = function(event){
     if (event.keyCode == 13 || event.which == 13){
@@ -42,7 +53,7 @@ function getProducts(data){
             <td>${prodName.replace(/\b\w/g, l => l.toUpperCase())}</td>
             <td>${prodDesc.replace(/\b\w/g, l => l.toUpperCase())}</td>
             <td>$ ${products.price}</td>
-            <td><a href="updateProduct.html" class="btn btn-secondary">Edit</a></td>
+            <td><button class="btn btn-secondary" onclick="updateProductModel('${prodId}','${prodName}','${prodDesc}','${prodPrice}')">Edit</button></td>
             <td><button class="btn btn-secondary" onclick="deleteProduct('${prodId}')">Delete</button></td>
         `;
         tabla.appendChild(row);
@@ -55,7 +66,6 @@ function cleanBody(){
 }
 
 async function deleteProduct(id){
-    console.log(`deleted id ${id}`);
     await fetch('https://spring-boot-mongo-db.herokuapp.com/deleteProduct/'+id, {
         method: 'DELETE',
         headers: {
@@ -68,8 +78,73 @@ async function deleteProduct(id){
     });
 }
 
-function reloadPage(){
-    location.reload(true);
+function editProduct(){
+
+}
+
+function addProduct(){
+    const url = 'https://spring-boot-mongo-db.herokuapp.com/saveProduct';
+    
+    var data = { name: document.getElementById("addNameProduct").value,
+                 description: document.getElementById("addDescriptionProduct").value,
+                 price: document.getElementById("addPriceProduct").value
+                };
+
+    fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(cleanForm());
+    /*  .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
+      
+*/
+}
+
+function cleanForm(){
+    document.getElementById("addNameProduct").value="";
+    document.getElementById("addDescriptionProduct").value = "";
+    document.getElementById("addPriceProduct").value = "";
+    alert("Saved");
+    closeModal();
+    traerDatos();
 }
 
 traerDatos();
+
+
+/*-------------MODALS*---------------------*/
+
+// When the user clicks the button, open the modal 
+function updateProductModel(id, name, description, price) {
+    console.log(id);
+    document.getElementById("updateNameProduct").value = name;
+    document.getElementById("updateDescriptionProduct").value = description;
+    document.getElementById("updatePriceProduct").value = price;
+    modalUpdate.style.display = "block";
+}
+
+function closeModal(){
+    modalUpdate.style.display = "none";
+    modalAdd.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalUpdate) {
+    modalUpdate.style.display = "none";
+  }
+}
+
+//------------------------------------------------
+function addProductModel() {
+    modalAdd.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalAdd) {
+    modalAdd.style.display = "none";
+  }
+}
